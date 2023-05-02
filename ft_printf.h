3f2edb850c763883b9f4af5a8dcbf191ad6fdd48
Francisco Miguel Da Estrela Inacio <fda-estr@c1r3s6.42lisboa.com>
2023-05-02 19:59:50 +0100

    load

---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fda-estr <fda-estr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/02 18:56:40 by fda-estr          #+#    #+#             */
+/*   Updated: 2023/05/02 19:50:59 by fda-estr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
